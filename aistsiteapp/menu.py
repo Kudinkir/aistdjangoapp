c71c5a292169a30061813e2e58ceb20b745e421e
KKudinov <kudinov_kirill@mail.ru>
2019-09-11 22:41:10 +0400

    add menu

--- aistsiteapp/menu.py
+++ aistsiteapp/menu.py
@@ -5,12 +5,12 @@ from datetime import date
 def menu(request):
     event_block = Events.objects.filter(start_date__gte=date.today())
     courses_block = VideoCourses.objects.all()
-    footer_block = Blocks.objects.get(id=7)
-    footer_block.bloks = Blocks.objects.filter(block_id=7)
     main_menu = MenuBlocks.objects.get(slug='top_menu')
     main_menu.items = main_menu.get_items()
+    footer_menu = MenuBlocks.objects.get(slug='bottom_menu')
+    footer_menu.items = footer_menu.get_items()
     return {
-        'footer_block' : footer_block,
+        'footer_menu' : footer_menu,
         'event_block'  : event_block,
         'courses_block'  : courses_block,
         'main_menu' : main_menu
