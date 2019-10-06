9da212a80eaa986df165b31237280c68db0370d9
KKudinov <kudinov_kirill@mail.ru>
2019-10-07 00:10:34 +0400

    fix tinymce js&css

--- aistproject/settings.py
+++ aistproject/settings.py
@@ -80,14 +80,23 @@ TINYMCE_DEFAULT_CONFIG = {
     'custom_undo_redo_levels': 20,
     'selector': 'textarea',
     'theme': 'modern',
-    'plugins': "table,spellchecker,paste,searchreplace, image,codesample,contextmenu,code",
-    # 'plugins': '''
-    #         textcolor save link image media preview codesample contextmenu
-    #         table code lists fullscreen  insertdatetime  nonbreaking
-    #         contextmenu directionality searchreplace wordcount visualblocks
-    #         visualchars code fullscreen autolink lists  charmap print  hr
-    #         anchor pagebreak
-    #         ''',
+    'plugins': '''
+            textcolor save link image media preview codesample contextmenu
+            table code lists fullscreen  insertdatetime  nonbreaking
+            contextmenu directionality searchreplace wordcount visualblocks
+            visualchars code fullscreen autolink lists  charmap print  hr
+            anchor pagebreak
+            ''',
+    'toolbar1': '''
+            fullscreen preview bold italic underline | fontselect,
+            fontsizeselect  | forecolor backcolor | alignleft alignright |
+            aligncenter alignjustify | indent outdent | bullist numlist table |
+            | link image media | codesample |
+            ''',
+    'toolbar2': '''
+            visualblocks visualchars |
+            charmap hr pagebreak nonbreaking anchor |  code |
+            ''',
     'contextmenu': 'formats | link image',
     'menubar': True,
     'statusbar': True,
