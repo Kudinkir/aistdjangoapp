cde98047923430711471ff54fca1c2685b053b85
KKudinov <kudinov_kirill@mail.ru>
2019-10-06 21:15:40 +0400

    fix tinymce js&css

--- aistproject/settings.py
+++ aistproject/settings.py
@@ -88,16 +88,6 @@ TINYMCE_DEFAULT_CONFIG = {
     #         visualchars code fullscreen autolink lists  charmap print  hr
     #         anchor pagebreak
     #         ''',
-    'toolbar1': '''
-            fullscreen preview bold italic underline | fontselect,
-            fontsizeselect  | forecolor backcolor | alignleft alignright |
-            aligncenter alignjustify | indent outdent | bullist numlist table |
-            | link image media | codesample |
-            ''',
-    'toolbar2': '''
-            visualblocks visualchars |
-            charmap hr pagebreak nonbreaking anchor |  code |
-            ''',
     'contextmenu': 'formats | link image',
     'menubar': True,
     'statusbar': True,
