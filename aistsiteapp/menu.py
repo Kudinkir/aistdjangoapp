7016345de9e3708e2bf3fc7fd20a5e8ac3ab162c
KKudinov <kudinov_kirill@mail.ru>
2019-10-22 16:15:15 +0400

    add footer_text_menu + edit template

--- aistsiteapp/menu.py
+++ aistsiteapp/menu.py
@@ -6,6 +6,8 @@ def menu(request):
     event_block = Events.objects.filter(start_date__gte=date.today())
     courses_block = VideoCourses.objects.all()
     footer_info = Blocks.objects.get(id = 19)
+    footer_text_menu = MenuBlocks.objects.get(slug='text_footer_menu')
+    footer_text_menu.items = footer_text_menu.get_items()
     main_menu = MenuBlocks.objects.get(slug='top_menu')
     main_menu.items = main_menu.get_items()
     footer_menu = MenuBlocks.objects.get(slug='bottom_menu')
@@ -15,5 +17,6 @@ def menu(request):
         'event_block'  : event_block,
         'courses_block'  : courses_block,
         'main_menu' : main_menu,
-        'footer_info' : footer_info
+        'footer_info' : footer_info,
+        'footer_text_menu':footer_text_menu
     }
