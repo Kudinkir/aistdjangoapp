7783a49a708aa0884cac2ea6b48a111edeb6c7f7
Nikita <nikitsidenk@mail.ru>
2019-10-23 13:16:41 +0300

    callback form / styles fixes

--- js/components/contactForm.js
+++ js/components/contactForm.js
@@ -1,5 +1,16 @@
 export default class ContactForm {
     constructor(context, componentName) {
-        console.log(componentName, '🚀');
+        this.context = context;
+        this.context.addEventListener('submit', this.onSubmit)
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+        const body = new FormData(this.context);
+
+        fetch('/callback', { method: 'POST', body })
+            .finally(() => this.context.reset())
+            .then(resp => resp.json())
+            .catch(console.log)
     }
 }
